75f0d07be385b6e4e71775dcb465ede686f8bd06
Mariia Kulikova <mariia.kulikova@deliveryhere.com>
2025-07-21 17:09:17 +0200

    refactor(Product) remove unused logic

--- src/main/kotlin/ecommerce/Product.kt
+++ src/main/kotlin/ecommerce/Product.kt
@@ -5,19 +5,4 @@ class Product(
     var name: String,
     var price: Double,
     var imageUrl: String,
-) {
-    fun update(newProduct: Product) {
-        this.name = newProduct.name
-        this.price = newProduct.price
-        this.imageUrl = newProduct.imageUrl
-    }
-
-    companion object {
-        fun toEntity(
-            product: Product,
-            id: Long,
-        ): Product {
-            return Product(id = id, product.name, product.price, product.imageUrl)
-        }
-    }
-}
+)
