e41499c3109cffd81709e99ebb0efcaea499327c
Mariia Kulikova <mariia.kulikova@deliveryhere.com>
2025-07-28 20:01:24 +0200

    test: fix auth controller test
    exclude WebConfig class from application setup, mock argument resolver and interceptor

--- src/test/kotlin/ecommerce/controller/AuthControllerTest.kt
+++ src/test/kotlin/ecommerce/controller/AuthControllerTest.kt
@@ -2,20 +2,32 @@ package ecommerce.controller
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.ninjasquad.springmockk.MockkBean
+import ecommerce.auth.AdminOnlyInterceptor
+import ecommerce.auth.LoginMemberArgumentResolver
+import ecommerce.config.WebConfig
+import ecommerce.dto.Member
 import ecommerce.dto.TokenRequest
 import ecommerce.dto.TokenResponse
 import ecommerce.service.AuthService
 import io.mockk.every
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
-import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.FilterType
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.post
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(
+    AuthController::class,
+    excludeFilters = [
+        ComponentScan.Filter(
+            type = FilterType.ASSIGNABLE_TYPE,
+            classes = [WebConfig::class],
+        ),
+    ],
+)
 class AuthControllerTest {
     @Autowired
     private lateinit var mockMvc: MockMvc
@@ -26,16 +38,31 @@ class AuthControllerTest {
     @MockkBean
     private lateinit var authService: AuthService
 
+    @MockkBean
+    private lateinit var adminOnlyInterceptor: AdminOnlyInterceptor
+
+    @MockkBean
+    private lateinit var loginMemberArgumentResolver: LoginMemberArgumentResolver
+
     @Test
     fun `should return token when login is successful`() {
         val request = TokenRequest("user@email.com", "1234")
         val expectedResponse = TokenResponse("mocked-token")
 
         every {
-            authService.login(match {
-                it.email == "user@email.com" && it.password == "1234"
-            })
+            authService.login(
+                match {
+                    it.email == "user@email.com" && it.password == "1234"
+                },
+            )
         } returns expectedResponse
+
+        every { adminOnlyInterceptor.preHandle(any(), any(), any()) } returns true
+        every { loginMemberArgumentResolver.resolveArgument(any(), any(), any(), any()) } returns
+            Member(
+                email = "a@a",
+                password = "123",
+            )
 
         mockMvc.post("/api/members/login") {
             contentType = MediaType.APPLICATION_JSON
@@ -54,10 +81,19 @@ class AuthControllerTest {
         val expectedResponse = TokenResponse("registered-token")
 
         every {
-            authService.register(match {
-                it.email == "user@email.com" && it.password == "1234"
-            })
+            authService.register(
+                match {
+                    it.email == "user@email.com" && it.password == "1234"
+                },
+            )
         } returns expectedResponse
+
+        every { adminOnlyInterceptor.preHandle(any(), any(), any()) } returns true
+        every { loginMemberArgumentResolver.resolveArgument(any(), any(), any(), any()) } returns
+            Member(
+                email = "a@a",
+                password = "123",
+            )
 
         mockMvc.post("/api/members/register") {
             contentType = MediaType.APPLICATION_JSON
