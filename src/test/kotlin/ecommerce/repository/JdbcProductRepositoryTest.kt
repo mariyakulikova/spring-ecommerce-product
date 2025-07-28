1f7fbc5892664218bd78ef81d457afdfd5a05778
Mariia Kulikova <mariia.kulikova@deliveryhere.com>
2025-07-28 19:23:47 +0200

    test: fix sql query typo

--- src/test/kotlin/ecommerce/repository/JdbcProductRepositoryTest.kt
+++ src/test/kotlin/ecommerce/repository/JdbcProductRepositoryTest.kt
@@ -20,7 +20,7 @@ class JdbcProductRepositoryTest {
         jdbcProductStore = JdbcProductRepository(jdbcTemplate)
 
         jdbcTemplate.execute("DROP TABLE IF EXISTS cart_items")
-        jdbcTemplate.execute("DROP TABLE products IF EXISTS")
+        jdbcTemplate.execute("DROP TABLE IF EXISTS products")
         jdbcTemplate.execute(
             "CREATE TABLE products(" +
                 "id BIGINT AUTO_INCREMENT, name VARCHAR(255) NOT NULL, price DOUBLE NOT NULL, image_url VARCHAR(512) NOT NULL)",
